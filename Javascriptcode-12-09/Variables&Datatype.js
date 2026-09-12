383f76a382e8a8926a96e9cb3ce12ef41bc1ebe5
ankitaborade13-jpg <ankitaborade13@gmail.com>
2026-09-12 19:50:20 +0530

    updated code

---
+++ Javascriptcode-12-09/Variables&Datatype.js
@@ -0,0 +1,28 @@
+// Q1
+// let name = "Ankita";
+// let age = 20;
+// let percentage = 90.5;
+
+// console.log("Name:", name);
+// console.log("Age:", age);
+// console.log("Percentage:", percentage);
+
+//Q2.
+// let name = "Ankita Borade";
+// const college = "Sanghavi College";
+// var course = "Computer Science";
+
+// console.log("Name:", name);
+// console.log("College:", college);
+// console.log("Course:", course);
+
+// Q3
+// let name = "Ankita";       
+// let age = 21;              
+// let isPass = true;      
+// let arr=[10,20,30];               
+
+// console.log(typeof name);
+// console.log(typeof age);
+// console.log(typeof isPass);
+// console.log(typeof arr);
---
+++ Javascriptcode-12-09/function.js
@@ -0,0 +1,17 @@
+// Q14
+function add(a, b) {
+    return a + b;
+}
+let sum = add(10, 20);
+console.log("Sum:", sum);
+
+// Q15
+function checkEvenOdd(number) {
+    if (number % 2 == 0) {
+    console.log("Number is Even");
+    }
+    else {
+    console.log("Number is Odd");
+    }
+}
+checkEvenOdd(10);
