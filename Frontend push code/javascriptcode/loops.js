40d1474cfb3fdceba614187e79cc37d6718106ed
ankitaborade13-jpg <ankitaborade13@gmail.com>
2026-09-12 18:45:33 +0530

    Javascript code 12-09

---
+++ Frontend push code/javascriptcode/loops.js
@@ -0,0 +1,17 @@
+// Q11
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+//Q12
+for (let i = 2; i <= 20; i = i + 2) {
+    console.log(i);
+}
+
+// Q13
+let i = 10;
+
+while (i >= 1) {
+    console.log(i);
+    i--;
+}
