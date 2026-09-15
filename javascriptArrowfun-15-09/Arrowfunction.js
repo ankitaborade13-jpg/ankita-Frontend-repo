e39f0cceb4efe08e8fa53b15f93a75a3d38d8321
ankitaborade13-jpg <ankitaborade13@gmail.com>
2026-09-15 15:49:36 +0530

    JSArrowfunction-15-09

---
+++ javascriptArrowfun-15-09/Arrowfunction.js
@@ -0,0 +1,37 @@
+// Q1
+// const hello = () => {
+//     console.log("Hello JavaScript");
+// };
+// hello();
+
+// Q2
+// const add = (a, b) => {
+//     return a + b;
+// };
+// console.log(add(10, 20));
+
+// Q3
+// const square = (num) => {
+//     return num * num;
+// };
+// console.log(square(5));
+
+// Q4
+// const checkEvenOdd = (num) => {
+//     if (num % 2 === 0) {
+//         console.log("Number is Even");
+//     } else {
+//         console.log("Number is Odd");
+//     }
+// };
+// checkEvenOdd(6);
+
+// Q5
+// const Result = (marks) => {
+//     if (marks >= 35) {
+//           console.log("Pass");
+//     } else {
+//         console.log("Fail");
+//     }
+// };
+// (Result(40));
