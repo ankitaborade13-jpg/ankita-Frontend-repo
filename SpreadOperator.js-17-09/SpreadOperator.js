6e2ad8354b0c8a8d5a15c5d56c0890c2e79389d1
ANKITA BORADE <ankitaborade13@gmail.com>
2026-09-17 12:58:58 +0530

    SpreadOpeartor-17-09

---
+++ SpreadOperator.js-17-09/SpreadOperator.js
@@ -0,0 +1,26 @@
+// Q1
+// let arrnum=[10,20,30,40,50];
+// let Copyarrnum =[...arrnum];
+// console.log(Copyarrnum);
+
+// Q2
+// let arr1=[1,2,3,4];
+// let arr2=[5,6,7,8];
+// let addarr=[...arr1, ...arr2;
+// console.log(addarr);
+
+// Q3
+// let fruits = ["Apple", "Mango", "Banana"];
+// let newFruits = [...fruits, "Orange", "Jamun"];
+// console.log(newFruits);
+
+// Q4
+let student = {
+    name: "Ankita",
+    age: 20
+};
+let newStudent = {
+    ...student,
+    city: "Nashik"
+};
+console.log(newStudent);
